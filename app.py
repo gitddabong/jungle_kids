b9772f8fdc5d9204bf19fbb687ebf615f2ddb359
gitddabong <wonjong0701@naver.com>
2021-11-02 14:18:20 +0900

    메인 웹 페이지 일부 수정

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, jsonify, request
 import requests
 from bs4 import BeautifulSoup
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient 
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
+client = MongoClient('localhost', 27017)
+db = client.dbkids
 
 @app.route('/')
 def home():
@@ -14,6 +14,7 @@ def home():
 
 @app.route('/memo', methods=['POST'])
 def post_article():
+    '''
     # 1. 클라이언트로부터 데이터를 받기
     url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
     comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
@@ -39,13 +40,16 @@ def post_article():
     db.articles.insert_one(article)
 
     return jsonify({'result': 'success'})
+    '''
 
 @app.route('/memo', methods=['GET'])
 def read_articles():
+    '''
     # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기 (Read)
     result = list(db.articles.find({}, {'_id': 0}))
     # 2. articles라는 키 값으로 article 정보 보내주기
     return jsonify({'result': 'success', 'articles': result})
+    '''
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

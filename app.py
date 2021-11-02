ba19632b61474460f333813fe176d7ec4fb7f1fa
gitddabong <wonjong0701@naver.com>
2021-11-03 00:01:13 +0900

    jinja2로 카드 만들기 실패
    카드에 각자 id를 부여할 수 없어서 포기

--- app.py
+++ app.py
@@ -10,46 +10,41 @@ db = client.dbkids
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # 여기서 등록된 달을 기준으로 오름차순 정렬을 하는데, 연도가 적혀있지 않아 가장 빨리 들어온 의뢰인지 정확히 알 수 없으므로 질문태클을 받을 수 있으니 이 부분은 설명을 해야 함.
+    result = list(db.boyuk_requests.find({}, {'_id': 0}))
+
+    #return render_template('index.html')
+    return render_template('index.html', results=result)
+
 
 @app.route('/memo', methods=['POST'])
 def post_article():
-    '''
-    # 1. 클라이언트로부터 데이터를 받기
-    url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
-    comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
+    title_receive = request.form['title_give']
+    startMonth_receive = request.form['startMonth_give']
+    startDay_receive = request.form['startDay_give']
+    startHour_receive = request.form['startHour_give']
+    endHour_receive = request.form['endHour_give']
+    comment_receive = request.form['comment_give']
+    boyuk_requests = {
+        'title': title_receive, 
+        'startMonth': startMonth_receive, 
+        'startDay': startDay_receive, 
+        'startHour': startHour_receive, 
+        'endHour': endHour_receive, 
+        'comment': comment_receive
+    }
 
-    # 2. meta tag를 스크래핑하기
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    og_image = soup.select_one('meta[property="og:image"]')
-    og_title = soup.select_one('meta[property="og:title"]')
-    og_description = soup.select_one('meta[property="og:description"]')
-
-    url_title = og_title['content']
-    url_description = og_description['content']
-    url_image = og_image['content']
-
-    article = {'url': url_receive, 'title': url_title, 'desc': url_description, 'image': url_image,
-               'comment': comment_receive}
-
-    # 3. mongoDB에 데이터를 넣기
-    db.articles.insert_one(article)
+    db.boyuk_requests.insert_one(boyuk_requests)
 
     return jsonify({'result': 'success'})
-    '''
 
+"""
 @app.route('/memo', methods=['GET'])
 def read_articles():
-    '''
-    # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기 (Read)
-    result = list(db.articles.find({}, {'_id': 0}))
-    # 2. articles라는 키 값으로 article 정보 보내주기
-    return jsonify({'result': 'success', 'articles': result})
-    '''
+    result = list(db.boyuk_requests.find({}, {'_id': 0}))
+    #return jsonify({'result': 'success', 'articles': result})
+    return render_template('index.html', results=result)
+"""
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

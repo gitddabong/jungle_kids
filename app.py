b489a4f4ab94c67035f5a60a3fda9a50823ba864
gitddabong <wonjong0701@naver.com>
2021-11-03 11:53:16 +0900

    글 수정 기능까지 완료
    이제 수락 기능과 보모 선택기능 추가 필요

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, jsonify, request
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient 
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
@@ -10,15 +12,13 @@ db = client.dbkids
 
 @app.route('/')
 def home():
-    # 여기서 등록된 달을 기준으로 오름차순 정렬을 하는데, 연도가 적혀있지 않아 가장 빨리 들어온 의뢰인지 정확히 알 수 없으므로 질문태클을 받을 수 있으니 이 부분은 설명을 해야 함.
-    result = list(db.boyuk_requests.find({}, {'_id': 0}))
+    db.boyuk_requests.delete_one
 
-    #return render_template('index.html')
-    return render_template('index.html', results=result)
-
+    return render_template('main.html')
 
 @app.route('/memo', methods=['POST'])
 def post_article():
+    writter_receive = request.form['writter_give']
     title_receive = request.form['title_give']
     startMonth_receive = request.form['startMonth_give']
     startDay_receive = request.form['startDay_give']
@@ -26,6 +26,7 @@ def post_article():
     endHour_receive = request.form['endHour_give']
     comment_receive = request.form['comment_give']
     boyuk_requests = {
+        'writter': writter_receive,
         'title': title_receive, 
         'startMonth': startMonth_receive, 
         'startDay': startDay_receive, 
@@ -35,16 +36,34 @@ def post_article():
     }
 
     db.boyuk_requests.insert_one(boyuk_requests)
+    return jsonify({'result': 'success'})
+
+@app.route('/update', methods=['POST'])
+def update_article():
+    writter_receive = request.form['writter_give']
+    documentId_receive = request.form['documentId_give']
+    title_receive = request.form['update_title_give']
+    startMonth_receive = request.form['update_startMonth_give']
+    startDay_receive = request.form['update_startDay_give']
+    startHour_receive = request.form['update_startHour_give']
+    endHour_receive = request.form['update_endHour_give']
+    comment_receive = request.form['update_comment_give']
+
+    db.boyuk_requests.update_one(
+        {'_id':ObjectId(documentId_receive)},
+        {'$set':{'title':title_receive, 'startMonth':startMonth_receive, 'startDay':startDay_receive, 'startHour':startHour_receive, 'endHour':endHour_receive, 'comment':comment_receive}}
+    )
 
     return jsonify({'result': 'success'})
 
-"""
 @app.route('/memo', methods=['GET'])
 def read_articles():
-    result = list(db.boyuk_requests.find({}, {'_id': 0}))
-    #return jsonify({'result': 'success', 'articles': result})
-    return render_template('index.html', results=result)
-"""
+    result = list(db.boyuk_requests.find({}))
+
+    for document in result:
+        document['_id']=str(document['_id'])
+
+    return jsonify({'result': 'success', 'articles': result})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
